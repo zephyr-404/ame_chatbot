772fb8535f8731fcb9116c67a425dafa57b36ccd
zephyr-404 <dimaeliason@gmail.com>
2025-02-25 21:40:04 +0100

    Add files via upload
    Double-click this to install every module you need for the program.

---
+++ shell_module_install.bat
@@ -0,0 +1,8 @@
+pip install google-generativeai
+pip install python-dotenv
+pip install requests
+pip install Flask
+pip install twilio
+pip install google-api-python-client
+pip install google-auth
+pip install pytz
